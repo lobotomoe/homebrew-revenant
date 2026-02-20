48b9e96cc40b0eba8ee9fea8647e9b4319bcb1db
Alex Kraiz <selfsurfer@gmail.com>
2026-02-20 22:53:09 +0000

    revenant 0.2.2

--- Formula/revenant.rb
+++ Formula/revenant.rb
@@ -2,10 +2,10 @@ class Revenant < Formula
   desc "Electronic document signing with ARX CoSign (SOAP/DSS)"
   homepage "https://github.com/lobotomoe/revenant"
   license "Apache-2.0"
-  version "0.2.1"
+  version "0.2.2"
 
   url "https://github.com/lobotomoe/revenant/releases/download/v#{version}/revenant-cli-macos-arm64"
-  sha256 "89be66df3cb21f1db9035104f532c2eb39a8e65c34a6eb4698e67e395527b3f4"
+  sha256 "244789c3e9e4526597cb1e042d6e678a3b3505f561ccc5b9571b460d0f68b744"
 
   depends_on :macos
   depends_on arch: :arm64
